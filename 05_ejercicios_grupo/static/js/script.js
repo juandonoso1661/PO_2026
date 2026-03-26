584a81a2ac2cc4ab8a69bd8156b0176410a0273a
juandonoso1661 <juandonoso@liceovvh.cl>
2026-03-26 15:10:46 -0300

    avanze

---
+++ 05_ejercicios_grupo/static/js/script.js
@@ -0,0 +1,16 @@
+console.log("conectado");
+
+/*Ejercicio 1: Función con validación de datos
+Objetivo: Validar entradas antes de procesarlas.
+Instrucciones:
+Crea un input donde el usuario escriba un número.
+Crea una función verificarNumero.
+La función debe:
+Capturar el valor del input
+Verificar si está vacío
+Si está vacío → mostrar: "Debes ingresar un número"
+Si tiene valor → mostrar: "Número ingresado correctamente" */
+
+function validacionDatos() {
+    const container = document.getElementById("container1")   
+}
